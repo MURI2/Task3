900205abdbc19bcfda7cff424f7fe9258b37f458
Megan Behringer <megbehri@indiana.edu>
2018-05-25 07:38:12 -0800

    Create Kallisto_Rpal.sh

---
+++ RNA-Seq/Kallisto_Rpal.sh
@@ -0,0 +1,19 @@
+cd /N/dc2/projects/muri2/Task3/RNA_Seq/Kallisto/ 
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o Rpal/4005a_A_S1_Quant --single -l 80 -s 10 GSF1180A-4005a-MG1655-A_S1_R1_001.fastq  
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o Rpal/4005_B_S2_Quant --single -l 80 -s 10 GSF1180A-4005-1655-B_S2_R1_001.fastq 
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o Rpal/4005_C_S3_Quant --single -l 80 -s 10 GSF1180A-4005-1655-C_S3_R1_001.fastq 
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o 4021_D_S32_Quant --single -l 80 -s 10 ../GSF1180B-4021-D_S32_R1_001.fastq
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o 4021_E_S33_Quant --single -l 80 -s 10 ../GSF1180B-4021-E_S33_R1_001.fastq
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o 4021_F_S34_Quant --single -l 80 -s 10 ../GSF1180B-4021-F_S34_R1_001.fastq
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o 4026_G_S35_Quant --single -l 80 -s 10 ../GSF1180B-4026-G_S35_R1_001.fastq
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o 4026_H_S36_Quant --single -l 80 -s 10 ../GSF1180B-4026-H_S36_R1_001.fastq
+
+../../../Tools/kallisto_linux-v0.43.0/kallisto quant -i Rpal_CDS.idx -o 4026_I_S37_Quant --single -l 80 -s 10 ../GSF1180B-4026-I_S37_R1_001.fastq
