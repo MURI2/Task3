456ea83392484d5642be20cddf02a90427a96cb6
Megan Behringer <megbehri@indiana.edu>
2018-05-25 07:34:01 -0800

    Create RNA_Assembly_Rpal.sh

---
+++ RNA-Seq/RNA_Assembly_Rpal.sh
@@ -0,0 +1,19 @@
+cd /N/dc2/projects/muri2/task3/RNA_Seq/CoCulture
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4005-1655-B_S2 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4005-1655-B_S2_R1_001.fastq
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4005-1655-C_S3 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4005-1655-C_S3_R1_001.fastq
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4021-1655-D_S4 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4021-1655-D_S4_R1_001.fastq
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4021-1655-E_S5 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4021-1655-E_S5_R1_001.fastq
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4021-1655-F_S6 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4021-1655-F_S6_R1_001.fastq
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4026-1655-G_S7 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4026-1655-G_S7_R1_001.fastq
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4026-1655-H_S8 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4026-1655-H_S8_R1_001.fastq
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4026-1655-I_S9 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4026-1655-I_S9_R1_001.fastq
+
+tophat2 -p 4 -i 30 -I 20000 -o Rpal/4005a-1655-A_S1 -G ../Ref_Genome/R_Pal/R_pal.RefGenome.gff ../Ref_Genome/R_Pal/R_pal.RefGenome.fa GSF1180A-4005a-MG1655-A_S1_R1_001.fastq
